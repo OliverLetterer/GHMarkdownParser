66cda5af958190ce73d2125560c428340ac4f1f9
Oliver Letterer <oliver.letterer@googlemail.com>
2011-08-01 17:31:35 +0200

    added raw static library project

---
+++ GHMarkdownParser/GHMarkdownParser/GHMarkdownParser.h
@@ -0,0 +1,13 @@
+//
+//  GHMarkdownParser.h
+//  GHMarkdownParser
+//
+//  Created by Oliver Letterer on 01.08.11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GHMarkdownParser : NSObject
+
+@end
---
+++ GHMarkdownParser/GHMarkdownParser/GHMarkdownParser.m
@@ -0,0 +1,23 @@
+//
+//  GHMarkdownParser.m
+//  GHMarkdownParser
+//
+//  Created by Oliver Letterer on 01.08.11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import "GHMarkdownParser.h"
+
+@implementation GHMarkdownParser
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
