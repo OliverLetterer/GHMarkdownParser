73ed8ea09d54b8b9cff4a5084e9f02f8f710cafa
Jens Alfke <jens@couchbase.com>
2014-03-09 14:37:15 -0700

    Updated to latest Discount. Added more options to the API.

--- GHMarkdownParser/GHMarkdownParser/GHMarkdownParser.m
+++ GHMarkdownParser/GHMarkdownParser/GHMarkdownParser.m
@@ -9,45 +9,68 @@
 #import "GHMarkdownParser.h"
 #import "markdown.h"
 
+// Declared in mkdio.h, but we can't include that after including markdown.
+extern void mkd_with_html5_tags(void);
+
+
 @implementation GHMarkdownParser
 
+@synthesize options=_options, githubFlavored=_githubFlavored, baseURL=_baseURL;
+
++ (void) initialize {
+    // Enable recognition of HTML5 tags
+    mkd_with_html5_tags();
+}
+
 + (NSString *)HTMLStringFromMarkdownString:(NSString *)markdownString {
-    NSString *HTMLString = nil;
-    
-    char *markdownUTF8 = (char*)markdownString.UTF8String;
-    Document *document = mkd_string(markdownUTF8, strlen(markdownUTF8), 0);
-    if (document && mkd_compile(document, 0)) {
+    GHMarkdownParser* parser = [[self alloc] init];
+    NSString* html = [parser HTMLStringFromMarkdownString:markdownString];
+#if !__has_feature(objc_arc)
+    [parser release];
+#endif
+    return html;
+}
+
+
++ (NSString *)flavoredHTMLStringFromMarkdownString:(NSString *)markdownString {
+    GHMarkdownParser* parser = [[self alloc] init];
+    parser.githubFlavored = YES;
+    NSString* html = [parser HTMLStringFromMarkdownString:markdownString];
+#if !__has_feature(objc_arc)
+    [parser release];
+#endif
+    return html;
+}
+
+
+- (NSString *)HTMLStringFromMarkdownString:(NSString *)markdownString {
+    NSData* mdData = [markdownString dataUsingEncoding:NSUTF8StringEncoding];
+    Document *document;
+    if (_githubFlavored)
+        document = gfm_string(mdData.bytes, mdData.length, _options);
+    else
+        document = mkd_string(mdData.bytes, mdData.length, _options);
+    if (!document)
+        return nil;
+
+    if (_baseURL)
+        mkd_basename(document, (char*)_baseURL.absoluteString.UTF8String);
+
+    NSString* html = nil;
+    if (mkd_compile(document, _options)) {
         char *HTMLUTF8 = NULL;
         int length = mkd_document(document, &HTMLUTF8);
-        if (length != EOF) {
-            HTMLString = [[NSString alloc] initWithBytes:HTMLUTF8
-                                                  length:length
-                                                encoding:NSUTF8StringEncoding];
+        if (length >= 0) {
+            html = [[NSString alloc] initWithBytes:HTMLUTF8
+                                            length:length
+                                          encoding:NSUTF8StringEncoding];
+#if !__has_feature(objc_arc)
+            [html autorelease];
+#endif
         }
         mkd_cleanup(document);
     }
-    
-    return HTMLString;
-}
-
-+ (NSString *)flavoredHTMLStringFromMarkdownString:(NSString *)markdownString {
-    NSMutableString *fixedMarkdown = [NSMutableString stringWithCapacity:markdownString.length];
-    __block BOOL isCodeBlock = NO;
-    [markdownString enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
-        if ([line rangeOfString:@"```"].location != NSNotFound) {
-            isCodeBlock = !isCodeBlock;
-        } else {
-            if (isCodeBlock) {
-                [fixedMarkdown appendFormat:@"      %@\n", line];
-            } else {
-                [fixedMarkdown appendFormat:@"%@\n", line];
-            }
-        }
-    }];
-    
-    NSString *HTML = [self HTMLStringFromMarkdownString:fixedMarkdown];
-    
-    return HTML;
+    return html;
 }
 
 @end
